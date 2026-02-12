23eea3d4f53a43027d410a41439c35b2a1138b73
unknown <asrorbeksaidmurodov2004@example.com>
2026-02-12 10:41:43 +0500

    Loyihada o'zgartirish qilindi

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-79zt-f9&y!s=!vuvc!)i^+)=@^63rj*5^4tc3(y=n2aqadr!p#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tashkent'
 
 USE_I18N = True
 
